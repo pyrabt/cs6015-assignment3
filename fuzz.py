1d1554e7ab13d198adf770454f2e8d9de8303138
pyrabt <wmarringa@gmail.com>
2019-02-02 20:41:14 -0700

    Now creating temp directory to write files to a test in
    Added more generators for random sized ascii string to
    test symbol checking.
    
    for now the classification is being run with for loops
    
    working on my generators before I start tackling the test oracles

--- fuzz.py
+++ fuzz.py
@@ -4,7 +4,10 @@ import os
 import random
 
 # define the name of the directory to be created
-path = "/tmp/testFiles"
+import shutil
+import string
+
+path = os.getcwd() + "/fuzzer_files"
 
 
 # create directory to do testing in
@@ -19,9 +22,19 @@ def start():
 
 # generate a randomly sized square
 def random_square():
-    side_len = random.randint(0, 101)
+    side_len = random.randint(1, 100)
     sq_coords = str(side_len) + " 0 " + str(side_len) + " " + str(side_len) + " 0 " + str(side_len)
     return sq_coords
+
+
+def generate_square_cases():
+    square_filenames = []
+    for x in range(0, 100):
+        name_of_file = "testSquare_" + str(x) + ".txt"
+        square_file = open("fuzzer_files/" + name_of_file, 'w')
+        square_file.write(random_square())
+        square_filenames.append(name_of_file)
+    return square_filenames
 
 
 # generate a randomly sized rectangle
@@ -29,27 +42,54 @@ def random_rectangle():
     side_len1 = 0
     side_len2 = 0
     while side_len1 is side_len2:
-        side_len1 = random.randint(0, 101)
-        side_len2 = random.randint(0, 101)
+        side_len1 = random.randint(1, 100)
+        side_len2 = random.randint(1, 100)
     sq_coords = str(side_len1) + " 0 " + str(side_len1) + " " + str(side_len2) + " 0 " + str(side_len2)
     return sq_coords
+
+
+def generate_rectangle_cases():
+    rect_filenames = []
+    for x in range(0, 100):
+        name_of_file = "testRect_" + str(x) + ".txt"
+        square_file = open("fuzzer_files/" + name_of_file, 'w')
+        square_file.write(random_rectangle())
+        rect_filenames.append(name_of_file)
+    return rect_filenames
+
+
+def random_ascii_string():
+    return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(0, 20))
+
+
+def generate_random_ascii_cases():
+    rand_ascii_filenames = []
+    for x in range(0, 300):
+        name_of_file = "testRandAscii_" + str(x) + ".txt"
+        random_file = open("fuzzer_files/" + name_of_file, 'w')
+        random_file.write(random_ascii_string())
+        rand_ascii_filenames.append(name_of_file)
+    return rand_ascii_filenames
 
 
 # remove temp directory
 def clean():
     try:
-        os.rmdir(path)
+        shutil.rmtree(path)
     except OSError:
         print ("Deletion of the directory %s failed" % path)
     else:
         print ("Successfully deleted the directory %s" % path)
 
 
-test_file = open("testSquare.txt", 'w')
-test_file.write(random_square())
-test_file.close()
-test_file2 = open("testRect.txt", 'w')
-test_file2.write(random_rectangle())
-test_file2.close()
-os.system("./classifier < testSquare.txt")
-os.system("./classifier < testRect.txt")
+start()
+
+for test_file in generate_square_cases():
+    os.system("./classifier < fuzzer_files/" + test_file)
+
+for test_file in generate_rectangle_cases():
+    os.system("./classifier < fuzzer_files/" + test_file)
+
+for test_file in generate_random_ascii_cases():
+    os.system("./classifier < fuzzer_files/" + test_file)
+clean()
